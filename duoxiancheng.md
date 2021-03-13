dae321068e4a2625831c531abdc8f071808f239f
13949 <1394949540@qq.com>
2021-03-13 22:33:34 +0800

    add

--- duoxiancheng.md
+++ duoxiancheng.md
@@ -1,4 +1,10 @@
 # 多线程高频面试考点
 ### Volatile关键字的作用和实现
+- 保证可见性
+- 不保证原子性
+- 避免指令重排(内存屏障) 再Volatile写的前后加锁避免指令重排
 [讲解的很详细](https://blog.csdn.net/zzti_erlie/article/details/86355477)
+### Volatile怎么保证原子性
+- 加Lock锁，Synchronized锁。
+- 使用原子类，解决原子性问题(AtomicInteger)
 ### 聊聊线程池你知道多少
--- src/main/java/ArraysSort.java
+++ src/main/java/ArraysSort.java
@@ -6,5 +6,11 @@ public class ArraysSort {
         int k = in.nextInt();
         int[] arr = {50,3,5,2,2};
         Arrays.sort(arr,0,5);
+        Integer v1 = 1;
+        Integer v2 = 1;
+        Integer v3 = 6666;
+        Integer v4 = 6666;
+        System.out.println(v1 == v2);
+        System.out.println(v3.equals(v4));
     }
 }
