a26981bf405e9b265477a9f95af8d71d7381f47a
13949 <1394949540@qq.com>
2021-03-31 12:22:18 +0800

    singleton

--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -1,8 +1,23 @@
 package singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class Singleton {
+    private static boolean flag = false;
+
     private static volatile Singleton singleton;
-    private Singleton(){}
+
+    private Singleton(){
+        synchronized (Singleton.class){
+            if (flag == false){
+                flag = true;
+            }else {
+                throw new RuntimeException("不要视图使用反射破坏单例");
+            }
+        }
+    }
     public static Singleton getInstance(){
         // 第一个if判断如果存在实例就直接返回，不然没有第一层的话进来就会加锁，影响性能。
         if (singleton == null){
@@ -11,18 +26,32 @@ public class Singleton {
                 // 等到锁释放后，如果不进行判断那么第二个线程又会创建出一个实例
                 if (singleton == null) {
                     singleton = new Singleton();
+                    /**
+                     * 1. 分配内存空间
+                     * 2. 执行构造函数，初始化对象
+                     * 3. 把这个对象指向分配的内存空间
+                     * 123 A ->
+                     * 132 发生指令重排。 B -> 拿到了一个虚无的对象，此时还没有完成构造！
+                     */
                 }
             }
         }
         return singleton;
     }
-    public static void main(String[] args) {
-        for (int i = 0; i < 30; i++) {
-            new Thread(() -> {
-                Singleton instance = Singleton.getInstance();
-                System.out.println(instance.hashCode());
-            },i+"").start();
-        }
+    // 反射破坏单例模式
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        //Singleton instance = Singleton.getInstance();
+        Field flag = Singleton.class.getDeclaredField("flag");
+        flag.setAccessible(true);
+        Constructor<Singleton> singletonConstructor = Singleton.class.getDeclaredConstructor(null);
+        singletonConstructor.setAccessible(true);
+        // 如果两个对象都是反射创建的，不走构造函数，则单例模式依旧会被破坏
+        // 加一个标志位
+        Singleton instance = singletonConstructor.newInstance();
+        // 单例又被破坏了
+        flag.set(instance,false);
+        Singleton instance2 = singletonConstructor.newInstance();
+        System.out.println(instance == instance2);
 
     }
 }
